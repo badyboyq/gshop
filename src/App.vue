a42b6bc67a7df78b9623e1dbca251ec10055c592
蔡昆 <2456757625@qq.com>
2018-06-25 15:17:37 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <footer-guide v-if="$route.meta.showFooter"> </footer-guide>
+  </div>
+</template>
+
+<script>
+  import  FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  components:{FooterGuide}
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+
+  color #2c3e50
+  width 100%
+  height 199%
+  background #f5f5f5
+  /*margin-top: 60px*/
+
+
+
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import  VueRouter from 'vue-router'
+Vue.use(VueRouter);
+import MSite from '../pages/MSite/MSite'
+import  Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+
+export  default  new VueRouter({
+routes:[
+  {
+    path:'/',
+    redirect:'msite'
+
+
+  },
+  {
+    path:'/msite',
+    component:MSite,
+    meta:{
+      showFooter:true
+    }
+  },
+  {
+    path:'/search',
+    component:Search,
+    meta:{
+      showFooter:true
+    }
+  },
+  {
+    path:'/order',
+    component:Order,
+    meta:{
+      showFooter:true
+    }
+  }, {
+    path:'/profile',
+    component: Profile,
+    meta:{
+      showFooter:true
+    }
+  }, {
+    path:'/login',
+    component:Login
+  }
+
+
+]
+
+})
